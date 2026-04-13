34c6aa76b84554f52e7cfaf1de4c1e2355e01d98
RafaelRCamposUnip <rafaeletec55@gmail.com>
2026-04-13 19:49:10 -0300

    Add files via upload

---
+++ verifica_idade.py
@@ -0,0 +1,7 @@
+#Verifica_idade.py
+nome=input ("Informe seu nome:")
+idade=int(input("Informe a sua idade:"))
+if idade >=18:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
