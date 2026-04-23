0d69f7b9804c4156c6feaa95c731e76ff998c7af
RafaelRCamposUnip <rafaeletec55@gmail.com>
2026-04-22 21:34:24 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-nomes=["Tamires", "Simone", "Mel"]
-for nome in nomes:
-   print(f"Olá, {nome}!")
+#def saudacao():
+    #print("Olá, bem-vindo!")
+#print = saudacao()
 
-amigos={"Mário":19, "Bruno":19}
-for nome, idade in amigos.items():
-    print(f"{nome} tem {idade} anos")
-
-num=int(input("Digite um número:"))
-for i in range(1,11):#1 a 10
-    print(f"{num} X {i}={num*i}")
+prossiga = "S"
+media = 0.0
+soma =0.0
+contador = 0
+while prossiga == "S":
+    num = float(input("Informe um número: "))
+    contador += 1
+    soma += num
+    prossiga = input("DESEJA PROSSEGUIR? S/N: ").upper()
+    if prossiga == "N":
+        print("Saindo...")
+media = soma/contador
+print("Contagem de números: ",contador)
+print("Soma: ",soma)
+print("Média: ",media)
