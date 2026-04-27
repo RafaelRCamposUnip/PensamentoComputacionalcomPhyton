df11a8460ceba1433c8e6d824b73f6a89990f625
RafaelRCamposUnip <rafaeletec55@gmail.com>
2026-04-27 20:35:45 -0300

    Add files via upload

---
+++ manipulacaodestrings.py
@@ -0,0 +1,25 @@
+#Concatenar
+S1="Renan"
+S2=" Rodrigues"
+Resultado=S1+S2
+print(Resultado)
+#Repetição
+S3="A"
+print(S3*10)
+#Comprimento(Length)
+S4="Python"
+print(len(S4))
+#Transformação
+S5="PyThOn"
+print(S5.lower())
+print(S5.upper())
+print(S5.title())
+print(S5.capitalize())
+#Substituição
+S6="Hello World"
+print(S6.replace("World", "Miguel"))
+#Remover espaços
+S7=" Hello "
+print(S7.strip())
+print(S7.lstrip())
+print(S7.rstrip())
