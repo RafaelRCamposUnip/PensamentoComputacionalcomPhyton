c269b6850a0c753e457529fb5d13dc987eb2665a
RafaelRCamposUnip <rafaeletec55@gmail.com>
2026-05-13 18:07:44 -0300

    Update sistema_pim.py

--- sistema_pim.py
+++ sistema_pim.py
@@ -1,4 +1,5 @@
 import csv
+import os
 producao = []
 estoque = []
 residuos = []
@@ -26,6 +27,8 @@ def registrar_producao():
         }
 
         producao.append(registro)
+
+        exportar_producao_csv()
 
         print("Produção registrada com sucesso!")
 
@@ -59,6 +62,9 @@ def registrar_estoque():
         for item in estoque:
             if item["material"].lower() == material.lower():
                 item["quantidade"] += quantidade
+
+                exportar_estoque_csv()
+
                 print("Estoque atualizado!")
                 return
 
@@ -66,6 +72,8 @@ def registrar_estoque():
             "material": material,
             "quantidade": quantidade
         })
+
+        exportar_estoque_csv()
 
         print("Material adicionado ao estoque!")
 
@@ -97,6 +105,9 @@ def registrar_residuo():
         for item in residuos:
             if item["tipo"].lower() == tipo.lower():
                 item["quantidade"] += quantidade
+
+                exportar_residuos_csv()
+
                 print("Resíduo atualizado!")
                 return
 
@@ -104,6 +115,8 @@ def registrar_residuo():
             "tipo": tipo,
             "quantidade": quantidade
         })
+
+        exportar_residuos_csv()
 
         print("Resíduo registrado com sucesso!")
 
@@ -119,6 +132,43 @@ def registrar_residuo():
 
             else:
                 print("Digite apenas 's' ou 'n'.")
+
+def carregar_producao_csv():
+    if os.path.exists("producao.csv"):
+
+        with open("producao.csv", "r", encoding="utf-8") as arquivo:
+            leitor = csv.DictReader(arquivo)
+
+            for linha in leitor:
+                producao.append({
+                    "data": linha["Data"],
+                    "produto": linha["Produto"],
+                    "quantidade": int(linha["Quantidade"])
+                })
+
+def carregar_estoque_csv():
+    if os.path.exists("estoque.csv"):
+
+        with open("estoque.csv", "r", encoding="utf-8") as arquivo:
+            leitor = csv.DictReader(arquivo)
+
+            for linha in leitor:
+                producao.append({
+                    "material": linha["Material"],
+                    "quantidade": int(linha["Quantidade"])
+                })
+
+def carregar_residuos_csv():
+    if os.path.exists("residuos.csv"):
+
+        with open("residuos.csv", "r", encoding="utf-8") as arquivo:
+            leitor = csv.DictReader(arquivo)
+
+            for linha in leitor:
+                producao.append({
+                    "tipo": linha["Tipo"],
+                    "quantidade": int(linha["Quantidade"])
+                })
 
 def listar_producao():
     if len(producao) == 0:
@@ -139,7 +189,7 @@ def listar_estoque():
 def listar_residuos():
     if len(residuos) == 0:
         print("Sem registro!")
-        return 
+        return
 
     for item in residuos:
         print(f"{item['tipo']} - {item['quantidade']}")
@@ -209,8 +259,8 @@ def exportar_producao_csv():
         print("Não há dados de produção para exportar!")
         return
 
-    with open("producao.csv", "w", newline="", encoding="utf-8") as arquivos:
-        escritor = csv.writer(arquivos)
+    with open("producao.csv", "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
         escritor.writerow(["Data", "Produto", "Quantidade"])
         for item in producao:
             escritor.writerow([
@@ -225,8 +275,8 @@ def exportar_estoque_csv():
         print("Não há dados do estoque para exportar!")
         return
 
-    with open("estoque.csv", "w", newline="", encoding="utf-8") as arquivos:
-        escritor = csv.writer(arquivos)
+    with open("estoque.csv", "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
         escritor.writerow(["Material", "Quantidade"])
         for item in estoque:
             escritor.writerow([
@@ -240,8 +290,8 @@ def exportar_residuos_csv():
         print("Não há dados de resíduos para exportar!")
         return
 
-    with open("residuos.csv", "w", newline="", encoding="utf-8") as arquivos:
-        escritor = csv.writer(arquivos)
+    with open("residuos.csv", "w", newline="", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
         escritor.writerow(["Tipo", "Quantidade"])
         for item in residuos:
             escritor.writerow([
@@ -249,6 +299,184 @@ def exportar_residuos_csv():
                 item["quantidade"]
             ])
     print("Arquivo 'residuos.csv' gerado com sucesso!")
+
+def buscar_producao():
+    if len(producao) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do produto: ").lower()
+
+    encontrado = False
+
+    for item in producao:
+        if item["produto"].lower() == busca:
+            print(f"{item['data']} - {item['produto']} - {item['quantidade']}")
+            encontrado = True
+
+        if not encontrado:
+            print("Produto não encontrado!")
+
+def buscar_estoque():
+    if len(estoque) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do material: ").lower()
+
+    encontrado = False
+
+    for item in estoque:
+        if item["material"].lower() == busca:
+            print(f"{item['material']} - {item['quantidade']}")
+            encontrado = True
+
+        if not encontrado:
+            print("Material não encontrado!")
+
+def buscar_residuos():
+    if len(residuos) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do resíduo: ").lower()
+
+    encontrado = False
+
+    for item in residuos:
+        if item["tipo"].lower() == busca:
+            print(f"{item['tipo']} - {item['quantidade']}")
+            encontrado = True
+
+        if not encontrado:
+            print("Resíduo não encontrado!")
+
+def editar_producao():
+    if len(producao) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do produto que deseja  editar: ").lower()
+
+    for item in producao:
+        if item["produto"].lower() == busca:
+            print("\nRegistro encontrado:")
+            print(f"Data: {item['data']}")
+            print(f"Produto: {item['produto']}")
+            print(f"Quantidade: {item['quantidade']}")
+
+            nova_data = input("Nova data: ")
+            novo_produto = input("Novo produto: ")
+            while True:
+                 try:
+                     nova_quantidade = int(input("Nova quantidade: "))
+                     break
+                 except ValueError:
+                     print("Digite apenas números!")
+
+                 item['data'] = nova_data
+                 item['produto'] = novo_produto
+                 item['quantidade'] = nova_quantidade
+
+                 exportar_producao_csv()
+
+                 print("Registro atualizado com sucesso!")
+                 return
+    print("Produto não encontrado!")
+
+def editar_estoque():
+    if len(estoque) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do material que deseja  editar: ").lower()
+
+    for item in estoque:
+        if item["material"].lower() == busca:
+            print("\nRegistro encontrado:")
+            print(f"Material: {item['material']}")
+            print(f"Quantidade: {item['quantidade']}")
+
+            novo_material = input("Novo material: ")
+            while True:
+                 try:
+                     nova_quantidade = int(input("Nova quantidade: "))
+                     break
+                 except ValueError:
+                     print("Digite apenas números!")
+
+                 item['material'] = novo_material
+                 item['quantidade'] = nova_quantidade
+
+                 exportar_estoque_csv()
+
+                 print("Registro atualizado com sucesso!")
+                 return
+    print("Material não encontrado!")
+
+def editar_residuos():
+    if len(residuos) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do resíduo que deseja  editar: ").lower()
+
+    for item in residuos:
+        if item["tipo"].lower() == busca:
+            print("\nRegistro encontrado:")
+            print(f"Resíduo: {item['tipo']}")
+            print(f"Quantidade: {item['quantidade']}")
+
+            novo_residuo = input("Novo resíduo: ")
+            while True:
+                 try:
+                     nova_quantidade = int(input("Nova quantidade: "))
+                     break
+                 except ValueError:
+                     print("Digite apenas números!")
+
+                 item['material'] = novo_residuo
+                 item['quantidade'] = nova_quantidade
+
+                 exportar_residuos_csv()
+
+                 print("Registro atualizado com sucesso!")
+                 return
+    print("Resíduo não encontrado!")
+
+def excluir_producao():
+    if len(producao) == 0:
+        print("Nenhum registro encontrado!")
+        return
+
+    busca = input("Digite o nome do produto que deseja  excluir: ").lower()
+
+    for item in producao:
+        if item["produto"].lower() == busca:
+            print("\nRegistro encontrado:")
+            print(f"Data: {item['data']}")
+            print(f"Produto: {item['produto']}")
+            print(f"Quantidade: {item['quantidade']}")
+
+            confirmar = input("Deseja excluir esse registro? (s/n): ").lower()
+
+            if confirmar == 's':
+                producao.remove(item)
+
+                exportar_producao_csv()
+
+                print("Registro excluido com sucesso!")
+                return
+
+            else:
+                print("Exclusão cancelada.")
+                return
+            
+    print("Produto não encontrado!")
+
+carregar_producao_csv()
+carregar_estoque_csv()
+carregar_residuos_csv()
 
 while True:
     print("\n--- SISTEMA ---")
@@ -260,9 +488,13 @@ while True:
     print("6- Ver resíduos")
     print("7- Relatório de produção")
     print("8- Relatório ambiental")
-    print("9- Exportar produção (CSV)")
-    print("10- Exportar estoque (CSV)")
-    print("11- Exportar resíduos (CSV)")
+    print("9- Buscar produção")
+    print("10- Buscar estoque")
+    print("11- Buscar resíduos")
+    print("12- Editar produção")
+    print("13- Editar estoque")
+    print("14- Editar resíduos")
+    print("15- Excluir produção")
     print("0- Sair")
 
     opcao = input("Escolha uma opção: ")
@@ -292,13 +524,25 @@ while True:
         relatorio_ambiental()
 
     elif opcao == "9":
-        exportar_producao_csv()
+        buscar_producao()
 
     elif opcao == "10":
-        exportar_estoque_csv()
+        buscar_estoque()
 
     elif opcao == "11":
-        exportar_residuos_csv()
+        buscar_residuos()
+
+    elif opcao == "12":
+        editar_producao()
+
+    elif opcao == "13":
+        editar_estoque()
+
+    elif opcao == "14":
+        editar_residuos()
+        
+    elif opcao == '15':
+        excluir_producao()
 
     elif opcao == "0":
         print("Encerrando sistema...")
